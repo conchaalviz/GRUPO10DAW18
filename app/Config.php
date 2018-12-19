8d2f62f40c3f999b0d2055ab669e501af6378e67
Vero Scardilli <verorumba77@gmail.com>
2018-12-19 12:51:18 +0100

    modificacion de datos de acceso a la bbdd

--- app/Config.php
+++ app/Config.php
@@ -4,8 +4,8 @@
  {
      static public $mvc_bd_hostname = "localhost";
      static public $mvc_bd_nombre   = "alimentos";
-     static public $mvc_bd_usuario  = "root";
-     static public $mvc_bd_clave    = "root";
+     static public $mvc_bd_usuario  = "GRUPO10DAW18";
+     static public $mvc_bd_clave    = "Despliegue2";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
